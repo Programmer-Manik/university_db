605140d1af162ce0731b35fff82ff5cc23c78f78
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-10-22 22:30:53 +0600

    Student table Create or Insert data

--- university_db.sql
+++ university_db.sql
@@ -1,1 +1,13 @@
 -- Active: 1723122519319@@127.0.0.1@5432@university_db
+
+--Students TABLE Create done  
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+    age INTEGER,
+    email VARCHAR(100) UNIQUE,
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(20)
+);
+
